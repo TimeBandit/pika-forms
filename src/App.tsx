9d465b197d2fe8eebe26dd9dbdd4c57075a47c83
timebandit <imran.nazir@yahoo.co.uk>
2020-11-03 20:36:39 +0000

    created hook to load data from gist

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import useLoadData from "./hooks/useLoadData";
 
 function App() {
+  const data = useLoadData() as { pages?: any[]; title?: string };
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <ul>{data.pages && data.pages.map((page, index) => <li key={index}>{page.type}</li>)}</ul>
         <a
           className="App-link"
           href="https://reactjs.org"
